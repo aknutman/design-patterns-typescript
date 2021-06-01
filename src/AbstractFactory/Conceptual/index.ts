c233819fb949efd37bb54169de55d2aa92bcab81
Eldest.Pasirula <aknutman@gmail.com>
2021-06-01 21:39:37 +0700

    test

--- src/AbstractFactory/Conceptual/index.ts
+++ src/AbstractFactory/Conceptual/index.ts
@@ -3,11 +3,6 @@
  *
  * Intent: Lets you produce families of related objects without specifying their
  * concrete classes.
- *
- * RU: Паттерн Абстрактная Фабрика
- *
- * Назначение: Предоставляет интерфейс для создания семейств связанных или
- * зависимых объектов без привязки к их конкретным классам.
  */
 
 /**
@@ -18,11 +13,6 @@
  * variants, but the products of one variant are incompatible with products of
  * another.
  *
- * RU: Интерфейс Абстрактной Фабрики объявляет набор методов, которые возвращают
- * различные абстрактные продукты. Эти продукты называются семейством и связаны
- * темой или концепцией высокого уровня. Продукты одного семейства обычно могут
- * взаимодействовать между собой. Семейство продуктов может иметь несколько
- * вариаций, но продукты одной вариации несовместимы с продуктами другой.
  */
 interface AbstractFactory {
     createProductA(): AbstractProductA;
@@ -36,10 +26,6 @@ interface AbstractFactory {
  * that signatures of the Concrete Factory's methods return an abstract product,
  * while inside the method a concrete product is instantiated.
  *
- * RU: Конкретная Фабрика производит семейство продуктов одной вариации. Фабрика
- * гарантирует совместимость полученных продуктов. Обратите внимание, что
- * сигнатуры методов Конкретной Фабрики возвращают абстрактный продукт, в то
- * время как внутри метода создается экземпляр конкретного продукта.
  */
 class ConcreteFactory1 implements AbstractFactory {
     public createProductA(): AbstractProductA {
@@ -54,7 +40,6 @@ class ConcreteFactory1 implements AbstractFactory {
 /**
  * EN: Each Concrete Factory has a corresponding product variant.
  *
- * RU: Каждая Конкретная Фабрика имеет соответствующую вариацию продукта.
  */
 class ConcreteFactory2 implements AbstractFactory {
     public createProductA(): AbstractProductA {
@@ -70,8 +55,6 @@ class ConcreteFactory2 implements AbstractFactory {
  * EN: Each distinct product of a product family should have a base interface.
  * All variants of the product must implement this interface.
  *
- * RU: Каждый отдельный продукт семейства продуктов должен иметь базовый
- * интерфейс. Все вариации продукта должны реализовывать этот интерфейс.
  */
 interface AbstractProductA {
     usefulFunctionA(): string;
@@ -80,7 +63,6 @@ interface AbstractProductA {
 /**
  * EN: These Concrete Products are created by corresponding Concrete Factories.
  *
- * RU: Эти Конкретные Продукты создаются соответствующими Конкретными Фабриками.
  */
 class ConcreteProductA1 implements AbstractProductA {
     public usefulFunctionA(): string {
@@ -99,9 +81,6 @@ class ConcreteProductA2 implements AbstractProductA {
  * interact with each other, but proper interaction is possible only between
  * products of the same concrete variant.
  *
- * RU: Базовый интерфейс другого продукта. Все продукты могут взаимодействовать
- * друг с другом, но правильное взаимодействие возможно только между продуктами
- * одной и той же конкретной вариации.
  */
 interface AbstractProductB {
     /**
@@ -117,10 +96,6 @@ interface AbstractProductB {
      * The Abstract Factory makes sure that all products it creates are of the
      * same variant and thus, compatible.
      *
-     * RU: ...а также взаимодействовать с Продуктами A той же вариации.
-     *
-     * Абстрактная Фабрика гарантирует, что все продукты, которые она создает,
-     * имеют одинаковую вариацию и, следовательно, совместимы.
      */
     anotherUsefulFunctionB(collaborator: AbstractProductA): string;
 }
@@ -128,7 +103,6 @@ interface AbstractProductB {
 /**
  * EN: These Concrete Products are created by corresponding Concrete Factories.
  *
- * RU: Эти Конкретные Продукты создаются соответствующими Конкретными Фабриками.
  */
 class ConcreteProductB1 implements AbstractProductB {
 
@@ -141,9 +115,6 @@ class ConcreteProductB1 implements AbstractProductB {
      * variant, Product A1. Nevertheless, it accepts any instance of
      * AbstractProductA as an argument.
      *
-     * RU: Продукт B1 может корректно работать только с Продуктом A1. Тем не
-     * менее, он принимает любой экземпляр Абстрактного Продукта А в качестве
-     * аргумента.
      */
     public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
         const result = collaborator.usefulFunctionA();
@@ -162,9 +133,6 @@ class ConcreteProductB2 implements AbstractProductB {
      * variant, Product A2. Nevertheless, it accepts any instance of
      * AbstractProductA as an argument.
      *
-     * RU: Продукт B2 может корректно работать только с Продуктом A2. Тем не
-     * менее, он принимает любой экземпляр Абстрактного Продукта А в качестве
-     * аргумента.
      */
     public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
         const result = collaborator.usefulFunctionA();
@@ -177,9 +145,6 @@ class ConcreteProductB2 implements AbstractProductB {
  * types: AbstractFactory and AbstractProduct. This lets you pass any factory or
  * product subclass to the client code without breaking it.
  *
- * RU: Клиентский код работает с фабриками и продуктами только через абстрактные
- * типы: Абстрактная Фабрика и Абстрактный Продукт. Это позволяет передавать
- * любой подкласс фабрики или продукта клиентскому коду, не нарушая его.
  */
 function clientCode(factory: AbstractFactory) {
     const productA = factory.createProductA();
@@ -192,7 +157,6 @@ function clientCode(factory: AbstractFactory) {
 /**
  * EN: The client code can work with any concrete factory class.
  *
- * RU: Клиентский код может работать с любым конкретным классом фабрики.
  */
 console.log('Client: Testing client code with the first factory type...');
 clientCode(new ConcreteFactory1());
